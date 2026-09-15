e8318bf9ed7d6fa3a14fdd7bfa0861e089a36240
Xuan Duy <moiduy04@gmail.com>
2026-09-15 12:55:18 +0700

    chore: continue removal of token-wasting header from files

--- 3rdparty/Automodel-workspace/Automodel/nemo_automodel/shared/import_utils.py
+++ 3rdparty/Automodel-workspace/Automodel/nemo_automodel/shared/import_utils.py
@@ -1,16 +1,4 @@
-# Copyright (c) 2024, NVIDIA CORPORATION.  All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+
 
 # This file is taken from https://github.com/NVIDIA/NeMo-Curator, which is adapted from cuML's safe_imports module:
 # https://github.com/rapidsai/cuml/blob/e93166ea0dddfa8ef2f68c6335012af4420bc8ac/python/cuml/internals/safe_imports.py
